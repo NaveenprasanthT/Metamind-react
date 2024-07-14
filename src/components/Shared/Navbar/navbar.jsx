241dda2dad5bac547d7953dfe92378bb35ffd2ff
Naveenprasanth07 <naveen.t@in.aictec.com>
2024-07-14 20:34:46 +0530

    responsiveness

--- src/components/Shared/Navbar/navbar.jsx
+++ src/components/Shared/Navbar/navbar.jsx
@@ -1,67 +1,111 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { motion } from 'framer-motion';
 import Button from '../../Shared/Button/button';
 import logo from '../../../assets/mmLogo.png';
 import './navbar.css'
+import { CloseIcon, ToggleIcon } from '../svgIcon';
 
 const Navbar = ({ activeSection }) => {
+    const [mobileToggle, setMobileToggle] = useState(false);
     const scrollToElementWithOffset = (id) => {
         const element = document.getElementById(id);
         if (element) {
             const elementPosition = element.getBoundingClientRect().top + window.pageYOffset;
-            const offsetPosition = elementPosition - 100;
+            const offsetPosition = elementPosition - 150;
 
             window.scrollTo({
                 top: offsetPosition,
                 behavior: 'smooth'
             });
         }
+        setMobileToggle(false);
     };
     return (
-        <div className='navbarContainer'>
-            <motion.div
-                className='navbarWrap'
-                initial={{ opacity: 0, y: 50, scale: 0.9 }}
-                whileInView={{ opacity: 1, y: 0, scale: 1 }}
-                transition={{ duration: 1 }}
-                viewport={{ once: true }}
-            >
-                <div className='navLogo'>
-                    <img alt='navbar_logo' src={logo} />
-                </div>
-                <div className='navbarList'>
-                    <ul className='menuLists'>
+        <>
+            <div className="empty-wrap" />
+            <div className='navbarContainer'>
+                <motion.div
+                    className='navbarWrap'
+                    initial={{ opacity: 0, y: 50, scale: 0.9 }}
+                    whileInView={{ opacity: 1, y: 0, scale: 1 }}
+                    transition={{ duration: 1 }}
+                    viewport={{ once: true }}
+                >
+                    <div className='navLogo'>
+                        <img alt='navbar_logo' src={logo} />
+                    </div>
+                    <div className='navbarList'>
+                        <ul className='menuLists'>
+                            <li
+                                onClick={() => scrollToElementWithOffset('main-section')}
+                                style={{ color: activeSection === 'main-section' && '#6633EE' }}
+                            >
+                                Home
+                            </li>
+                            <li
+                                onClick={() => scrollToElementWithOffset('about-section')}
+                                style={{ color: activeSection === 'about-section' && '#6633EE' }}
+                            >
+                                About
+                            </li>
+                            <li
+                                onClick={() => scrollToElementWithOffset('service-section')}
+                                style={{ color: activeSection === 'service-section' && '#6633EE' }}
+                            >
+                                Service
+                            </li>
+                            <li
+                                onClick={() => scrollToElementWithOffset('our-works-section')}
+                                style={{ color: activeSection === 'our-works-section' && '#6633EE' }}
+                            >
+                                Portfolio
+                            </li>
+                        </ul>
+                    </div>
+                    <div className='navbarBtn'>
+                        <Button buttonTxt={"Contact US"} handleClick={() => { }} />
+                    </div>
+                    <div className="mobile-toggle">
+                        <span onClick={() => setMobileToggle(true)}>
+                            <ToggleIcon />
+                        </span>
+                    </div>
+                </motion.div>
+                <div
+                    className="mobile-menus"
+                    style={{ right: mobileToggle === false ? '-100vw' : '0' }}
+                >
+                    <ul>
                         <li
                             onClick={() => scrollToElementWithOffset('main-section')}
-                            style={{ color: activeSection === 'main-section' && '#6633EE' }}
                         >
                             Home
                         </li>
                         <li
                             onClick={() => scrollToElementWithOffset('about-section')}
-                            style={{ color: activeSection === 'about-section' && '#6633EE' }}
                         >
                             About
                         </li>
                         <li
                             onClick={() => scrollToElementWithOffset('service-section')}
-                            style={{ color: activeSection === 'service-section' && '#6633EE' }}
                         >
                             Service
                         </li>
                         <li
                             onClick={() => scrollToElementWithOffset('our-works-section')}
-                            style={{ color: activeSection === 'our-works-section' && '#6633EE' }}
                         >
                             Portfolio
                         </li>
+                        <li
+                        // onClick={() => scrollToElementWithOffset('our-works-section')}
+                        >
+                            Contact Us
+                        </li>
                     </ul>
+                    <div className="close-icon" onClick={() => setMobileToggle(false)}><CloseIcon /></div>
                 </div>
-                <div className='navbarBtn'>
-                    <Button buttonTxt={"Contact US"} handleClick={() => { }} />
-                </div>
-            </motion.div>
-        </div>
+            </div>
+        </>
     )
 }
 
